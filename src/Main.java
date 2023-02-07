4aed8a0f36d68c7bf018229bebd15d4847a6dedd
karimullina <123@>
2023-02-07 17:55:17 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        Phone phone1 = new Phone();
+        Phone phone2 = new Phone();
+        Phone phone3 = new Phone();
+        System.out.println(phone1.number + phone1.model + phone1.weight);
+        System.out.println(phone2.number + phone2.model + phone2.weight);
+        System.out.println(phone3.number + phone3.model + phone3.weight);
     }
     public static class Phone {
         int number;
