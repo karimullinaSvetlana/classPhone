15bf1dcc2f9ea025c0f619c7d9178dfa1854d9ed
karimullina <123@>
2023-02-07 17:51:36 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public static class Phone {
+        int number;
+        String model;
+        int weight;
+    }
+}
