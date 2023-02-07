37df938254e37e4a694e029caf4dc80d0393b088
karimullina <123@>
2023-02-07 20:15:08 +0300

    3

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,22 @@ public class Main {
         System.out.println(phone1.number + phone1.model + phone1.weight);
         System.out.println(phone2.number + phone2.model + phone2.weight);
         System.out.println(phone3.number + phone3.model + phone3.weight);
+        phone1.receiveCall("Паша");
+        phone2.receiveCall("Саша");
+        phone3.receiveCall("Наташа");
+        phone1.getNumber();
+        phone2.getNumber();
+        phone3.getNumber();
     }
     public static class Phone {
         int number;
         String model;
         int weight;
+        public void receiveCall(String name){
+            System.out.println("Звонит "+ name);
+        }
+        public void getNumber() {
+            System.out.println(this.number);
+        }
     }
 }
